66066504790d7c051e21f72f88cc9ce6eb32280d
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-09-18 15:29:14 -0400

    Added Button to Toggle Sidebar

--- src/App.vue
+++ src/App.vue
@@ -70,6 +70,17 @@
         </ul>
     </nav>
 
+    <div id="content">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <button type="button" id="sidebarCollapse" class="btn btn-info">
+            <i class="fas fa-align-left"></i>
+            <span></span>
+          </button>
+        </div>
+      </nav>
+    </div>
+
     <!-- Page Content -->
     <div id="content">
         <!-- We'll fill this with dummy content -->
@@ -87,6 +98,14 @@ export default {
     HelloWorld
   }
 };
+
+$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});
 
 </script>
 
